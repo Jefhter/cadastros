3a932f2068927eb66fc5ca25202850884bc69c35
jefhter <jefhtercabral@gmail.com>
2024-12-05 15:59:34 -0300

    Atualização Menus

--- códigos/main.c
+++ códigos/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <locale.h>
 
 // Observações:
 // - o vetor de alunos deve ser alocado dinamicamente, com a dimensão inicial 10, através de uma função;
@@ -35,7 +36,7 @@ typedef struct{
 typedef struct {
     int ra;
     char nome[100];
-    Data matrícula;
+    Data matricula;
     Disciplina disciplinas[4];
     char email[100];
 } Aluno;
@@ -59,7 +60,16 @@ typedef struct {
 //   b - para cada disciplina deve haver 4 notas, com o respectivo cálculo da sua média
 // //recebe um vetor do tipo td_aluno e sua dimensão
 void cadastraAluno(){
+    system("cls");
+    // int i, n;
+    // //Aluno alunos = alocaVetorAlunos();
+    // for(i=0; i<n; i++){
+    //     printf("Cadastro do Aluno %i", i+1);
+    //     printf("Informe o RA do Aluno: ");
+    //     //scanf("%i", &alunos.ra);
 
+
+    // }
 }
 
 //2 - validar data - não deve ser permitido o cadastro de uma data que não seja válida
@@ -81,32 +91,57 @@ int verificaHorario(){
 //   b - caso um aluno possua dois compromissos na mesma data, os horários não podem estar sobrepostos/coincidentes
 //recebe um vetor do tipo td_compromisso e sua dimensão
 void cadastraCompromisso(){
+    system("cls");
 
 }
 
 //5 - imprimir os dados de aluno:
 //recebe um vetor do tipo aluno e sua dimensão
 void imprimeVetorAlunos(){
+    system("cls");
+}
 
+void imprimeDadosAluno(){
+    system("cls");
+    
+}
+
+void imprimeCompromisso(char tipo){
+    switch (tipo){
+        case 'a':
+            break;
+        case 'b':
+            break;
+        case 'c':
+            break;
+        case 'd':
+            break;
+        default:
+            printf("Erro!");
+    }
 }
 
 // 6 – alocar dinamicamente o vetor de alunos
-void alocaVetorAulunos(){
+void alocaVetorAlunos(){
+
 
 }
 
 // 7 – realocar dinamicamente o vetor de alunos
 void realocaVetorAlunos(){
 
+
 }
 
 // 8 – alocar dinamicamente o vetor de compromissos
 void alocaVetorCompromissos(){
 
+
 }
 
 // 9 – realocar dinamicamente o vetor de compromissos
 void realocaVetorcompromissos(){
+
 
 }
 
@@ -124,8 +159,17 @@ void leData(){
 void menu(){
     char resp[2];
     do{
-        printf("Funcionalidades do Sistema:\n1 - Cadastrar Aluno\n2 - Cadastrar Compromisso\n3 - Imprimir Compromissos\n\ta - de um aluno\n\tb - de todos os alunos\n\tc - de uma data\n\td - de todas as datas\n4 - Imprimir dados de um aluno\n5 - Imprimir dados de todos os alunos\n6 - Sair");
+        //imprime o menu
+        printf("Funcionalidades do Sistema:\n-------------------------------------------------------\n\t1 - Cadastrar Aluno\n\t2 - Cadastrar Compromisso\n\t3 - Imprimir Compromissos\n\t\ta - de um aluno\n\t\tb - de todos os alunos\n\t\tc - de uma data\n\t\td - de todas as datas\n\t4 - Imprimir dados de um aluno\n\t5 - Imprimir dados de todos os alunos\n\t6 - Sair\n-------------------------------------------------------");
+        
+        // pega o número corespondente à funcionalidade e armaneza em resp
+        printf("\nO que deseja fazer? ");
         fgets(resp, sizeof(resp), stdin);
+
+        //teste de erro de resp
+
+        // analisa qual número o usuário respondeu e chama a função correspondente à funcionalidade
+        // nos compromissos avalia a segunda parte da variável e manda na função a especificação
         switch (resp[0]){
             case '1':
                 cadastraAluno();
@@ -135,6 +179,8 @@ void menu(){
                 break;
             case '3':
                 switch (resp[1]){
+                    // armazenar outro resp
+
                     case 'a':
                         imprimeCompromisso('a');
                         break;
@@ -147,6 +193,8 @@ void menu(){
                     case 'd':
                         imprimeCompromisso('d');
                         break;
+                    default:
+                        printf("Resposta Inválida.");
                 }
             case '4':
                 imprimeDadosAluno();
@@ -158,15 +206,30 @@ void menu(){
                 printf("Obrigado por usar meu sistema!\nVolte sempre!!");
                 break;
             default:
-                printf("Resposta Inválida.");
+                system("cls");
+                printf("Resposta (%s%s) Inválida!!\n\tFormato correto de resposta:\n\tNúmero correspondente a primeira opção seguido de Letra correspondente à especificação do Compromisso Caso nao haja, usar apenas o número da primeira opção.\n\tSEM ESPAÇOS!\n", resp[0], resp[1]);
         }
     }while(resp[0]!='6');
-
 }
 
 int main(){
-    printf("Bem-vindo ao meu Trabalho de Algoritmos 2!!\n\tAluno: Jefhter Cabral\n\tRA: 2565390\n\tProfessor: Gabriel Gengivir\n\tProjeto: Sistema de Cadastro");
+    //Reconhece caracteres em pt-BR
+    setlocale(LC_ALL, "pt_BR.UTF-8");
+
+    //Apresenta o Trabalho
+    system("cls");
+    printf("\n------------------------------------------------------\nBem-vindo ao meu Trabalho de Algoritmos 2!!\n\tAluno: Jefhter Cabral\n\tRA: 2565390\n\tProfessor: Gabriel Canhadas Genvigir\n\tProjeto: Sistema de Cadastro\n------------------------------------------------------\n\n");
+    
+    //Limpa a tela
+    printf("Press Enter to continue... ");
+    char resp = getchar();
+    system("cls");
+    
+    //chama o menu
     menu();
-    printf("By: Jefhter R. Cabral - 2565390\n11/2024.");
+
+    //finalização do programa
+    system("cls");
+    printf("\n\n-------------------------------\nBy: Jefhter R. Cabral - 2565390\n11/2024.\n-------------------------------\n\n");
     return 0;
 }
